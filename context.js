d7d3fd20b1cfc9093bf9a653cd44d8cf93a0adb4
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-27 22:26:10 +0300

    Add setting in context and rename a few files

--- context.js
+++ context.js
@@ -16,7 +16,23 @@ module.exports = (Sequelize, config) => {
     options
   );
 
+  const Users = require("./modules/users/users.model")(Sequelize, sequelize);
+  const Roles = require("./modules/roles/roles.model")(Sequelize, sequelize);
+  const Posts = require("./modules/posts/posts.model")(Sequelize, sequelize);
+
+  // User <-> Role
+  Users.belongsToMany(Roles, { through: "userRoles" });
+  Roles.belongsToMany(Users, { through: "userRoles" });
+
+  // Post -> User
+  Posts.belongsTo(Users);
+  Users.hasMany(Posts);
+
   return {
+    users: Users,
+    roles: Roles,
+    posts: Posts,
+
     sequelize,
     Sequelize,
   };
