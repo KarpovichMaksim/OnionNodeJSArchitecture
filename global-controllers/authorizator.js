2855a68926bf54b7bad2a1cebb885a3437c8ddc6
Maksim <the.butterfly.incorporated@gmail.com>
2021-11-29 20:27:09 +0300

    Add global authorization  controller

---
+++ global-controllers/authorizator.js
@@ -0,0 +1,8 @@
+const wrap = require("../helpers/wrap");
+
+module.exports = (authorizationService) =>
+  wrap(async (req, res, next) => {
+    await authorizationService.checkPermissions(req.user, req.path);
+
+    next();
+  });
