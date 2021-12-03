dc2075e2f8b41364fc437231702d0d862506d97e
Maksim <the.butterfly.incorporated@gmail.com>
2021-12-03 23:57:39 +0300

    Change post model

--- modules/posts/posts.model.js
+++ modules/posts/posts.model.js
@@ -10,7 +10,6 @@ module.exports = (Sequelize, sequelize) => {
     content: Sequelize.STRING,
     date: Sequelize.DATEONLY,
     rating: Sequelize.INTEGER,
-
     draft: Sequelize.BOOLEAN,
   });
 };
